1a0c9d317a1e9c3d92f6c199a9725f7f0df60b12
o.gudkova <oegshop@gmail.com>
2022-07-12 18:52:07 +0400

    Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your name ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha" )
+{
+    Console.WriteLine("Horey! It`s Masha!");
+}
+else
+{Console.Write("Hello, ");
+Console.WriteLine (username);
+}
