90b000b4bbccaa6ccc8f4399ed3def191b3be31b
o.gudkova <oegshop@gmail.com>
2022-08-18 00:07:38 +0400

    Ex_36

---
+++ Ex_hw005_task36/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 36: Задайте одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19   [-4, -6, 89, 6] -> 0
+
+int[] getArray(int length)
+{
+    Random rnd = new Random();
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(-10, 10);
+    }
+    return arr;
+}
+
+void printArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int getOddsSumm(int[] arr)
+{
+    int summ = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        summ += arr[i];
+    }
+    return summ;
+}
+
+int[] arr = getArray(4);
+printArray(arr);
+int summ = getOddsSumm(arr);
+Console.WriteLine($"\n{summ}");
