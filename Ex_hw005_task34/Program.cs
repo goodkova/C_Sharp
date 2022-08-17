bd6092a2cdaa35ed1c75a35011cd2a4bce66ad10
o.gudkova <oegshop@gmail.com>
2022-08-17 23:52:16 +0400

    Ex_34

---
+++ Ex_hw005_task34/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 34: Задайте массив заполненный 
+// случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] getArray(int length)
+{
+    Random rnd = new Random();
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+void printArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int getEvens(int[] arr)
+{
+    int evens = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            evens++;
+        }
+    }
+    return evens;
+}
+
+int[] arr = getArray(4);
+printArray(arr);
+int evens = getEvens(arr);
+Console.WriteLine($"\n{evens}");
