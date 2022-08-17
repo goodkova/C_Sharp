6070117c2215e4091695d89e44190348831f32b5
o.gudkova <oegshop@gmail.com>
2022-08-17 23:22:15 +0400

    Ex_29

--- Ex_hw004_task29/Program.cs
+++ Ex_hw004_task29/Program.cs
@@ -4,7 +4,7 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] getArray(int length) //
+int[] getArray(int length) 
 {
     Random rnd = new Random();
     int[] arr = new int[length];
